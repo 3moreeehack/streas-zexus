151a6b1af083e2a4b4733ab4487b1c208a43c7de
3moreeehack <51297963+3moreeehack@users.noreply.github.com>
2019-07-04 23:07:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('ready', () => {
 
 
 
-    const dreamv2 = ["544551132307914752"]
+    const dreamv2 = ["470314895850733570"]
     client.on('message', message => {
         var dream = message.content.split(` `).slice(1).join(' ');
           if (!dreamv2.includes(message.author.id)) return;
